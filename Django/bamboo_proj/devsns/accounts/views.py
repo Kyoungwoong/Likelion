b196fb3e06cdb1c3ed81f8dbab364eaa538e7121
Kyoungwoong <kyy980708@gmail.com>
2022-06-01 21:21:29 +0900

    대나무숲 프로젝트

---
+++ Django/bamboo_proj/devsns/accounts/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
+
+# Create your views here.
+def login(request):
+    # request의 method가 post이면 
+    # 로그인 하기
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(request, username=username, password =password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('index')
+        else:
+            return render(request, 'bad_login.html')
+
+    # request의 method가 get이면 
+    # login.html띄우기
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['repeat']:
+            # 회원가입
+            new_user = User.objects.create_user(username = request.POST['username'], password = request.POST['password'])            
+            # 로그인
+            auth.login(request, new_user)
+            # 홈 리다이렉션
+            return redirect('index')
+    return render(request,'register.html')
+   
